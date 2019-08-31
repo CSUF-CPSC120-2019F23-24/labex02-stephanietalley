3e89aced78ec52a1c10dddfed5a9a1b8f1c58612
stephanietalley <stephanietalley@cs104u-4h.acad.fullerton.edu>
2019-08-31 13:30:28 -0700

    creates a diamond pattern

--- prob01/main.cpp
+++ prob01/main.cpp
@@ -1,10 +1,17 @@
-// Name: 
+// Name:
 // This program displays a diamond pattern to the screen.
 
 #include <iostream>
 
 int main()
 {
+  std::cout << "   *" << std::endl;
+  std::cout << "  ***" << std::endl;
+  std::cout << " *****" << std::endl;
+  std::cout << "*******" << std::endl;
+  std::cout << " *****" << std::endl;
+  std::cout << "  ***" << std::endl;
+  std::cout << "   *" << std::endl;
 
   return 0;
 }
